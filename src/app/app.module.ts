2ec4ae407ef43c7448105e55750bb105b907035d
sridharpettelapersonal <sridharpettela83@gmail.com>
2026-01-03 23:23:38 -0500

    icon fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { 
+  personCircleOutline, 
+  addOutline, 
+  peopleOutline, 
+  callOutline, 
+  locationOutline, 
+  businessOutline, 
+  checkmarkCircleOutline, 
+  chevronDownOutline, 
+  chevronUpOutline, 
+  logOutOutline, 
+  lockClosedOutline 
+} from 'ionicons/icons';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Register all icons used in the app
+addIcons({
+  'person-circle-outline': personCircleOutline,
+  'add-outline': addOutline,
+  'people-outline': peopleOutline,
+  'call-outline': callOutline,
+  'location-outline': locationOutline,
+  'business-outline': businessOutline,
+  'checkmark-circle-outline': checkmarkCircleOutline,
+  'chevron-down-outline': chevronDownOutline,
+  'chevron-up-outline': chevronUpOutline,
+  'log-out-outline': logOutOutline,
+  'lock-closed-outline': lockClosedOutline
+});
 
 @NgModule({
   declarations: [AppComponent],
