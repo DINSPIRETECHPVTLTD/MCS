2c5c4e93091bd0b38e771cd674cd98c8b6ea8124
sridharpettela <sridharpettela@dinspire.in>
2026-02-05 08:33:11 -0500

    fixes

--- src/app/components/header-menu/header-menu.component.ts
+++ src/app/components/header-menu/header-menu.component.ts
@@ -274,8 +274,8 @@ export class HeaderMenuComponent implements OnInit {
       this.showLoanSubmenu = false;
       this.activeMenu = menu;
       this.menuChange.emit(menu);
-      // Navigate directly for branch users
-      if (this.isBranchUser) {
+      // Navigate directly for branch users AND org owners in Branch Mode
+      if (this.isBranchUser || this.isBranchMode) {
         setTimeout(() => {
           this.selectSubmenu(menu);
         }, 0);
