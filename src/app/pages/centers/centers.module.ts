91ef8b35859748449c99a7b58e81fb6013d9bd5f
sridharpettela <sridharpettela@dinspire.in>
2026-02-25 21:11:39 -0500

    modify centers

--- src/app/pages/centers/centers.module.ts
+++ src/app/pages/centers/centers.module.ts
@@ -5,17 +5,6 @@ import { IonicModule } from '@ionic/angular';
 import { CentersPageRoutingModule } from './centers-routing.module';
 import { CentersPage } from './centers.page';
 import { HeaderMenuComponent } from '../../components/header-menu/header-menu.component';
-import { AddCenterModalComponent } from './add-center-modal.component';
-import { EditCenterModalComponent } from './edit-center-modal.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
 import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
@@ -24,20 +13,11 @@ import { AgGridModule } from 'ag-grid-angular';
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatCardModule,
-    MatDividerModule,
-    MatIconModule,
-    MatPaginatorModule,
-    MatTooltipModule,
-    MatButtonModule,
     CentersPageRoutingModule,
     HeaderMenuComponent,
     AgGridModule
   ],
-  declarations: [CentersPage, AddCenterModalComponent, EditCenterModalComponent]
+  declarations: [CentersPage]
 })
-export class CentersPageModule {}
+export class CentersPageModule { }
 
